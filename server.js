5754b13a50ee0d25fcd4d7d2784c26b3b5b512d9
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-11-17 14:31:50 -0700

    add node setup for backend repo

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { PORT = 3000, MONGODB_URL} = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.connection
+    .on("open", () => console.log("Mongoose Connected"))
+    .on("close", () => console.log("Mongoose Closed"))
+    .on("error", (err) => console.log(`Mongoose Error: ${err}`));
+
+app.get("/", (req, res) => {
+    res.send("Registry Home Route");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
