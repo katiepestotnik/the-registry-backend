a25ad1e9812f00c224e5ebead161703ffbb6dda5
Lydia Anzures <lydiaanzures@dfly-laMBP.local>
2021-11-28 11:08:27 -0500

    api but with shorter code in server.js to test

--- server.js
+++ server.js
@@ -1,35 +1,79 @@
-//tutorial from Alex Merced YouTube used for token and auth with JWT
-//https://www.youtube.com/watch?v=7laEQTjhe_E&t=280s
-//https://www.youtube.com/watch?v=R-JyaR0LSHw&t=806s
-//https://www.youtube.com/watch?v=8n0XjO79mIY
 require("dotenv").config();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGODB_URL} = process.env;
 const express = require("express");
 const app = express();
-const mongoose = require("./db/db");
-const cors = require("cors");
-const morgan = require("morgan");
-const AuthRouter = require('./controllers/user');
-const HolRouter = require('./controllers/hol');
-const WedRouter = require('./controllers/wed');
+const mongoose = require("mongoose");
+const axios = require("axios")
 
- //Middleware   
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.connection
+    .on("open", () => console.log("Mongoose Connected"))
+    .on("close", () => console.log("Mongoose Closed"))
+    .on("error", (err) => console.log(`Mongoose Error: ${err}`));
 
 app.get("/", (req, res) => {
-  res.json(req.payload);
+    res.send("Registry Home Route");
 });
-/////////////////////////////////////////
-//auth function/middleware
-app.use('/auth', AuthRouter)
-/////////////////////////////////////////
-//routes from controllers
-app.use('/wed-registry', WedRouter);
-app.use('/hol-registry', HolRouter);
 
 
+//api route
+
+app.get("/proxy/test", async (req, res) => {
+    try {
+      const response = await axios("https://openapi.etsy.com/v2/listings/active?includes=MainImage&api_key=5351youj6cfiov6nca92yf3i&tags=gift")
+      // console.log(response.data)
+  
+      //function that can get a random number from 1-25, 
+      //we'll use this to target a random result from our search results object!
+      function randomNumber(){
+        const randNum = Math.floor(Math.random() * 25)
+        return randNum
+      }
+       //categories response is an array, let's loop through the categories
+      let products = []
+    
+      for (let i=0; i<11; i++) {
+        //get random number which we will use as index
+        const randomIndex = randomNumber()
+        console.log(randomIndex)
+  
+        //get product name from API response 
+        const productName = response.data.results[randomIndex].title
+        console.log(productName)
+  
+        //get product URL from API response
+        const productURL = response.data.results[randomIndex].url
+        console.log(productURL)
+  
+        //get product image from API response
+        const productImage = response.data.results[randomIndex].MainImage.url_fullxfull
+        console.log(productImage)
+        products[i] = {
+            name: productName,
+            image: productImage,
+            url: productURL 
+          };
+      }
+      console.log(products);
+  
+  
+     
+  
+    
+      res.json(response.data)
+      
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+  
+  
+  
+  
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
